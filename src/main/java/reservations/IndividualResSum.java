2a73e644fe4662534d15b8129679313adb92d7c7
Jeffrey Martinez <martinezjf2@gmail.com>
2023-02-25 00:59:41 -0500

    received the data and passed them on the to individual reservation jsp

--- src/main/java/reservations/IndividualResSum.java
+++ src/main/java/reservations/IndividualResSum.java
@@ -28,21 +28,51 @@ public class IndividualResSum extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		String reservation_id = request.getParameter("reservation_id");
-		String check_in = request.getParameter("check_in");
-		String check_out = request.getParameter("check_out");
 		String adults = request.getParameter("adults");
 		String children = request.getParameter("children");
-		String room_type = request.getParameter("room_type");
-		String confirmation_code = request.getParameter("confirmation_code");
 		String instructions = request.getParameter("instructions");
+		String confirmation_code = request.getParameter("confirmation_code");
+		String check_in = request.getParameter("check_in");
+		String check_out = request.getParameter("check_out");
+		String points = request.getParameter("points");
+		String room_type = request.getParameter("room_type");
+		String hotel_id = request.getParameter("points");
+		String user_id = request.getParameter("points");
+		String city = request.getParameter("points");
+		String state = request.getParameter("points");
+		String zip = request.getParameter("points");
+		String picture = request.getParameter("points");
+		String hotel_name = request.getParameter("points");
+		String address = request.getParameter("points");
+		String phone_number = request.getParameter("points");
+		String place_id = request.getParameter("points");
+		String amenities = request.getParameter("points");
+		String descrip = request.getParameter("points");
+				
+
 		request.setAttribute("reservation_id", reservation_id);
-		request.setAttribute("check_in", check_in);
-		request.setAttribute("check_out", check_out);
 		request.setAttribute("adults", adults);
 		request.setAttribute("children", children);
-		request.setAttribute("room_type", room_type);
-		request.setAttribute("confirmation_code", confirmation_code);
 		request.setAttribute("instructions", instructions);
+		request.setAttribute("confirmation_code", confirmation_code);
+		request.setAttribute("check_in", check_in);
+		request.setAttribute("check_out", check_out);
+		request.setAttribute("points", points);
+		request.setAttribute("room_type", room_type);
+		request.setAttribute("hotel_id", hotel_id);
+		request.setAttribute("user_id", user_id);
+		request.setAttribute("city", city);
+		request.setAttribute("state", state);
+		request.setAttribute("zip", zip);
+		request.setAttribute("picture", picture);
+		request.setAttribute("hotel_name", hotel_name);
+		request.setAttribute("address", address);
+		request.setAttribute("phone_number", phone_number);
+		request.setAttribute("place_id", place_id);
+		request.setAttribute("amenities", amenities);
+		request.setAttribute("descrip", descrip);
+		
+		
 		request.getRequestDispatcher("individual-reservation.jsp").forward(request,response);
 	}
 
